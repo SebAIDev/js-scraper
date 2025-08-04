1a073967ef3af7e05b478836cfd9b6a6e0c6ea24
SebAIDev <your_email@example.com>
2025-08-04 11:40:39 -0400

    Switch to puppeteer with postinstall

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
-const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/scrape', async (req, res) => {
   const url = req.query.url;
-  if (!url) return res.status(400).send({ error: 'Missing ?url=' });
+  if (!url) {
+    return res.status(400).json({ error: 'Missing ?url=' });
+  }
 
-  let browser = null;
+  let browser;
 
   try {
-    browser = await chromium.puppeteer.launch({
-	args: chromium.args,
-	defaultViewport: chromium.defaultViewport,
-	executablePath: await chromium.executablePath,
-	headless: chromium.headless,
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
     const page = await browser.newPage();
